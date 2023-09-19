ca3302eaf3e114644529e1c928c6b02aa8c7d7a8
mohammadzohaib23 <mohammadzohaib2398@gmail.com>
2023-09-19 15:31:06 +0500

    latest commit

---
+++ app.js
@@ -0,0 +1,65 @@
+
+// Q 3
+for(var i =1;i<=10;i++){
+document.write(i + "<br><br>");
+}
+// Q 4
+
+userInput=+prompt("enter the value of which table you want");
+userInput2=+prompt("enter the value upto which extent you want that table");
+
+for(var i =1;i<=userInput2;i++){
+    document.write(userInput+"*"+i+"="+ userInput*i+"<br><br>");
+}
+
+// Q 05
+
+var fruits = ["Apple","banana","mango","orange","strawberry"];
+for(i=0;i<fruits.length;i++){
+document.write("elemnt of "+ fruits[i]+ " at " +i + "<br><br>");
+}
+
+// Q 06
+// a
+document.write("counting : ");
+
+for(i=1;i<=15;i++){
+document.write(i + "&nbsp");
+}
+document.write( "<br><br>");
+
+// b
+document.write("reverse : ");
+
+for(i=15;i>=1;i--){
+document.write(i + "&nbsp");
+}
+document.write( "<br><br>");
+// c
+document.write("even : ");
+
+for(var i =0;i<=20;i=i+2){
+    document.write(i+"&nbsp")
+}
+document.write( "<br><br>");
+
+// d
+document.write("odd : ");
+
+for(var i =1;i<=20;i=i+2){
+    document.write(i +"&nbsp");
+}
+document.write( "<br><br>");
+// e
+document.write("series : ");
+
+for(var i =0;i<=20;i=i+2){
+    document.write(i+"k"+"&nbsp")
+}
+document.write( "<br><br>");
+// Q 07
+
+
+
+
+    
